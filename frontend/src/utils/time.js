4759d62f7207092cc4940e8b5202df68a5c551a8
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-12-15 13:27:17 +0400

    Final ci/cd

--- frontend/src/utils/time.js
+++ frontend/src/utils/time.js
@@ -8,12 +8,13 @@ function startInterval() {
 
 async function saveTime() {
   const time = this.currentTime
-  const res = await fetch('/api/times', {
+  const res = await fetch('http://localhost:5000/times', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ time }),
   })
-
   const json = await res.json()
   if (json.insertId) {
     this.savedTimes.unshift({ id: json.insertId, time })
@@ -22,14 +23,15 @@ async function saveTime() {
 }
 
 async function deleteTime(id) {
-  const res = await fetch(`/api/time/${id}`, { 
+  const res = await fetch(`http://localhost:5000/time/${id}`, {
     method: 'DELETE',
   })
-
   const json = await res.json()
   if (json.affectedRows) {
-    this.savedTimes = this.savedTimes.filter((t) => t.id !== id)
-    this.$toast.error(`Time with ID ${id} was deleted`, { position: 'top-right' })
+    this.savedTimes = this.savedTimes.filter((savedTime) => savedTime.id !== id)
+    this.$toast.error(`Time with ID ${id} was deleted`, {
+      position: 'top-right',
+    })
   }
 }
 
